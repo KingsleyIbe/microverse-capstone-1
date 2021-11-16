73903ce2902ad5c0a29cf38c244bc98b48fff436
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-11-16 13:38:05 +0100

    Initial commit on branch

---
+++ script.js
@@ -0,0 +1,61 @@
+
+window.onload = () => {
+
+const guestSpeakers = [
+    {
+        image: './images/emma-gbenga.png',
+        name: 'Emma Gbenga',
+        role: 'Founder Open Source Africa',
+        bio: 'Formal UI designer at Google'
+    },
+    {
+        image: './images/charles-shady.png',
+        name: 'Charles Shady',
+        role: 'Full-stack developer',
+        bio: 'Ex-Professor at the University of Manchester'
+    },
+    {
+        image: './images/ajise-bonke.png',
+        name: 'Ajise Tolu',
+        role: 'Chairman of the Organisation',
+        bio: 'Ex-Professor at the University of Manchester'
+    },
+    {
+        image: './images/kalolo-hauf.png',
+        name: 'Ajise Tolu',
+        role: 'Chairman of the Organisation',
+        bio: 'Ex-Professor at the University of Manchester'
+    },
+    {
+        image: './images/mithi-debas',
+        name: 'Ajise Tolu',
+        role: 'Chairman of the Organisation',
+        bio: 'Ex-Professor at the University of Manchester'
+    },
+    {
+        image: './images/sam-omar',
+        name: 'Ajise Tolu',
+        role: 'Chairman of the Organisation',
+        bio: 'Ex-Professor at the University of Manchester'
+    },
+];
+
+function displaySpeakerInfo(speakerInfo) {
+    const speakers = 
+    `<div class="speaker-1">
+    <div class="speaker-image">
+        <img src=${speakerInfo.image} alt="speaker-1">
+    </div>
+    <div>
+        <h2>${speakerInfo.name}</h2>
+        <h3>${speakerInfo.role}</h3>
+        <h4>${speakerInfo.bio}</h4>
+    </div>
+</div>`;
+return speakers;
+}
+const guestSpeakerAtEvent = document.querySelector('#image-display');
+guestSpeakers.forEach((card) => {
+    guestSpeakerAtEvent.innerHTML += displaySpeakerInfo(card);
+});
+};
