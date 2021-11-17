6d72bbb48cd231477858c982f58c5e44233a7ad3
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-11-17 10:45:28 +0100

    Add menu popUp feature

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ const guestSpeakers = [
         bio: 'Ex-Professor at the University of Manchester'
     },
     {
-        image: './images/mithi-debas',
+        image: './images/mithi-debas.png',
         name: 'Mithi Debas',
         role: 'Chairman of the Organisation',
         bio: 'Ex-Professor at the University of Manchester'
     },
     {
-        image: './images/sam-omar',
+        image: './images/sam-omar.png',
         name: 'Sam Omar',
         role: 'Chairman of the Organisation',
         bio: 'Ex-Professor at the University of Manchester'
@@ -54,8 +54,41 @@ function displaySpeakerInfo(speakerInfo) {
 </div>`;
 return speakers;
 }
-const guestSpeakerAtEvent = document.querySelector('#image-display');
+const guestSpeakerAtEvent = document.querySelector('.image-display-1');
 guestSpeakers.forEach((card) => {
     guestSpeakerAtEvent.innerHTML += displaySpeakerInfo(card);
 });
 };
+
+const hamburger = document.querySelector('.menu1');
+const closeBtn = document.querySelector('.closeBtn');
+
+hamburger.addEventListener('click', () => {
+    console.log('clicked')
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'block';
+  });
+
+  close(closeBtn,'.mobile-menu');
+  closeBtn.addEventListener('click', () => {
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'none';
+    close(menu);
+  });
+
+  const backToMainPage = document.querySelector('.menuPopUp');
+  close(backToMainPage,'.mobile-menu');
+  backToMainPage.addEventListener('click', () => {
+    const menu = document.querySelector('.mobile-menu');
+    menu.style.display = 'none';
+  });
+
+   function close(menu, toBeClose){
+     menu.addEventListener('click' , () =>{
+        const value = document.querySelector(toBeClose);
+        value.style.display = 'none';
+     });
+  };
+
+//   const closeBtn1 = document.querySelector('.closeBtn-1');
+//   close(closeBtn1,'.mobile-project-details');
